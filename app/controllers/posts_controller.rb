2f903381b587e9970e6c6de64aa10758a4503497
kip <cyruskiprop254@gmail.com>
2020-04-21 12:55:57 +0300

    style the member feed section of the index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,9 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-  end
+   @posts = Post.all.order("created_at DESC")
+   @post = Post.new
+ end
 
   # GET /posts/1
   # GET /posts/1.json
